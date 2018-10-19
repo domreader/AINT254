11809957b9687842f86fdf7bedc6e128059ab97b
domreader <33025225+domreader@users.noreply.github.com>
2018-10-19 14:45:48 +0100

    Adding Metal ball and prototype code

---
+++ Drift Dodging/Assets/Scripts/transform attack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformattack : MonoBehaviour {
+
+    private Transform ball_Transform;
+
+    [SerializeField]
+    private int ballSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+          ball_Transform = transform;
+	}
+	
+    private void slideAttack()
+    {
+
+        int rollX;
+        int rollY;
+        int rollZ;
+
+        rollX = 5;
+        rollY = 5;
+        rollZ = 5;
+
+        int speedOfRoll = ballSpeed * rollY;
+
+        rollY = speedOfRoll;
+
+        ball_Transform.position += new Vector3(rollX,rollY,rollZ);
+
+
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        slideAttack();
+
+	}
+}
---
+++ Drift Dodging/Assets/transformMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformMove : MonoBehaviour {
+
+    private Transform ball_Transform;
+
+    [SerializeField]
+    private float ballSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        ball_Transform = transform;
+    }
+
+    private void slideAttack()
+    {
+
+        
+        float rollX;
+        float rollY;
+        float rollZ;
+
+        rollX = 0.1f;
+        rollY = 0.1f;
+        rollZ = 0.1f;
+
+        float speedOfRoll = rollX * ballSpeed;
+
+        rollX = speedOfRoll;
+
+        ball_Transform.position += new Vector3(rollX, rollY, rollZ);
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        slideAttack();
+
+    }
+}
