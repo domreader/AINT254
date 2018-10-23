0b60ac7c3389b5dc56ae927deb9225fc139a4cd1
Dominic Reader <33025225+domreader@users.noreply.github.com>
2018-10-23 11:36:36 +0100

    Finish of Demo Session 1

---
+++ Drift Dodging/Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public void StartGame()
+    {
+
+        SceneManager.LoadScene("Game");
+
+    }
+
+    public void EndGame()
+    {
+
+        SceneManager.LoadScene("Game Over");
+
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void EndApplication()
+    {
+        Application.Quit();
+    }
+
+}
