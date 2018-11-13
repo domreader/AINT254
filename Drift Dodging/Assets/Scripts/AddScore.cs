c11bc37cd913147074724c91043547dff4225e5b
Dominic Reader <33025225+domreader@users.noreply.github.com>
2018-11-13 10:50:17 +0000

    Adding Score to game

---
+++ Drift Dodging/Assets/Scripts/AddScore.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour {
+
+
+    private bool isInCollider;
+    public Text Score;
+    private int Number;
+    private int updatedNumber;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        isInCollider = false;
+
+        Number = 0;
+
+        updatedNumber = 0;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        isInCollider = true;
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        isInCollider = false;
+
+    }
+
+    void Update()
+    {
+        if(isInCollider == true)
+        {
+
+            Number = (updatedNumber++);
+
+
+        }
+
+        Score.text = ("Score : " + Number);
+
+    }
+
+
+}
