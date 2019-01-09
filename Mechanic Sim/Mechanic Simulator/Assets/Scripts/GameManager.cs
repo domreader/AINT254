f3e7f2cb1f7a11ccaa2128c45bb748063c17d5b1
domreader <33025225+domreader@users.noreply.github.com>
2019-01-09 20:38:40 +0000

    Adding Extra level to be able to test car

---
+++ Mechanic Sim/Mechanic Simulator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void GoToSettings()
+    {
+        SceneManager.LoadScene("Go To Settings");
+    }
+
+    public void GoToGarage()
+    {
+        SceneManager.LoadScene("Go To Garage");
+    }
+
+    public void GoToRoad()
+    {
+        SceneManager.LoadScene("Go To Room");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
---
+++ Mechanic Sim/Mechanic Simulator/Assets/exitLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exitLevel : MonoBehaviour
+{
+
+    Collider exitCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
